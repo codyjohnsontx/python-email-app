c83d6797f51c54ae686c1dee9693af7bcff13dd2
Cody Johnson <codyjohnsontx@gmail.com>
2023-07-30 18:59:40 -0500

    expand code to send custom emails to create customize emails for customers

--- email_sender.py
+++ email_sender.py
@@ -1,16 +1,20 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Cody Johnson'
 email['to'] = 'codyjohnsontx@gmail.com'
 email['subject'] = 'Hi, how are you?'
 
-email.set_content('I\'m a python developer')
+# Set the email body content using the template and substitute method
+email.set_content(html.substitute(name='John Smith'), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
-	smtp.ehlo()
-	smtp.starttls()
-	smtp.login('codyjohnsontx@gmail.com', 'qcqdbaundakyqstl')
-	smtp.send_message(email)	
-	print('all good boss')
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('codyjohnsontx@gmail.com', 'qcqdbaundakyqstl')
+    smtp.send_message(email)
+    print('all good boss')
